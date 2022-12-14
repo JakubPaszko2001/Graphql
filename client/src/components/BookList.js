3ef98b2031a370038c11112ccfa45062da7603d8
JakubPaszko2001 <jakubpaszko01@gmail.com>
2022-12-15 00:41:20 +0100

    dodac tylko dodawanie autora i style

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 function BookList() {
   const { loading, error, data } = useQuery(getBooksQuery);
+  const [selected, setSelected] = useState();
 
   function displayData() {
     if (loading) {
       return <div>Loading books...</div>;
     } else {
       return data.books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            key={book.id}
+            onClick={() => {
+              setSelected(book.id);
+            }}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   }
 
   return (
-    <ul id="book-list">
-      <li>{displayData()}</li>
-    </ul>
+    <div>
+      <ul id="book-list">
+        <li>{displayData()}</li>
+      </ul>
+      <BookDetails bookId={selected} />
+    </div>
   );
 }
 
