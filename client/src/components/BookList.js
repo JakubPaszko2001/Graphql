660fdf968a68aa1f28c85f6feecedb21eb690460
JakubPaszko2001 <jakubpaszko01@gmail.com>
2022-12-16 01:01:24 +0100

    troche style

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -6,21 +6,30 @@ import BookDetails from "./BookDetails";
 function BookList() {
   const { loading, error, data } = useQuery(getBooksQuery);
   const [selected, setSelected] = useState();
-
   function displayData() {
     if (loading) {
       return <div>Loading books...</div>;
     } else {
       return data.books.map((book) => {
+        let random;
+        function randomColor() {
+          random = Math.floor(Math.random() * 359);
+        }
+        randomColor();
+        console.log(random);
         return (
-          <li
+          <div
+            style={{
+              backgroundColor: `hsl(${random}, 100%, 65%)`,
+            }}
+            className="book"
             key={book.id}
             onClick={() => {
               setSelected(book.id);
             }}
           >
             {book.name}
-          </li>
+          </div>
         );
       });
     }
@@ -28,9 +37,10 @@ function BookList() {
 
   return (
     <div>
-      <ul id="book-list">
-        <li>{displayData()}</li>
-      </ul>
+      <div className="booksContainer">
+        <h1 className="title">Books on the shelf</h1>
+        {displayData()}
+      </div>
       <BookDetails bookId={selected} />
     </div>
   );
